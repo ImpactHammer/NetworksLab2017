83e7970ea394e0d4c71f39cbf79c5858befb84d1
impacthammer <impacthammer@yandex.ru>
2017-12-15 23:24:32 +0300

    fix UDP client

--- ind/msg_client_udp/main.cpp
+++ ind/msg_client_udp/main.cpp
@@ -2,22 +2,36 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <winsock2.h>
-#include <windows.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 #include <string.h>
+#include <string>
 #include <map>
+
+#include <pthread.h>
 
 
 /* message  : [header][options][message]
  *
  * [options]: {username}{message length} */
 
+#ifndef uint
+typedef unsigned int uint;
+#endif
 
-#define HEADER_LEN      8   // length of [header]
-#define OPT_UNAME_LEN   16  // length of {username} in [options]
-#define OPT_MSGLEN_LEN  2   // length of {message length} in [options]
-#define MSG_MAXLEN      256 // maximum length of [message]
+#define HEADER_LEN          8   // length of [header]
+#define OPT_USERNAME_LEN   16  // length of {username} in [options]
+#define OPT_MSGLEN_LEN      2   // length of {message length} in [options]
+#define MSG_MAXLEN          256 // maximum length of [message]
+#define MSG_ID_MINVALUE     0   // minimum value of message id
+#define MSG_ID_STEP         1   // incremental step of message id
+#define MSG_ID_LEN          sizeof(unsigned int)
+#define MSG_DATA_MAXLEN     MSG_MAXLEN - MSG_ID_LEN - OPT_USERNAME_LEN - OPT_MSGLEN_LEN
+
+#define PCG_MAXLEN          512 // maximum packege length
 
 #define CMD_AUTHORIZE_ME    "auth"
 #define CMD_SEND_MESSAGE    "msg"
@@ -31,242 +45,232 @@
 #define HDR_CLI_DISCONNECT          "disc_me"
 
 #define HDR_SRV_INCOMING_MESSAGE    "inc_msg"
-#define HDR_SRV_ERROR               "err_msg"
+#define HDR_SRV_ERROR               "error_msg"
 
 #define RC_CLIENT_CLOSING   1
 
-typedef std::pair<std::string, void*(*)(void*)> fname_function;
+#define CLI_PORT 0
+
+typedef std::pair<std::string, void*(*)(void*) > fname_function;
 
 int sockfd;
+struct sockaddr addr_server;
+struct sockaddr_in cli_addr;
 
-std::map<std::string, void*(*)(void*)> handlers;  // key: msg header; value: handler
+uint msgid_incoming; // current incoming message id
+uint msgid_outgoing; // current outgoing message id
 
-DWORD thread_wait_for_recv;
-DWORD thread_listen_cmd;
+char* received_message;
 
-HANDLE h_wait_for_recv;
-HANDLE h_listen_cmd;
+std::map<std::string, void*(*)(void*) > handlers; // key: msg header; value: handler
 
-int readn(int s, char* buf, int b_remain) {
-    int b_rcvd = 0;
-    int rc;
-    while(b_remain) {
-        rc = recv(s, buf + b_rcvd, b_remain, 0);
-        if (rc <= 0) {
-            return rc;
-        }
-        b_rcvd += rc;
-        b_remain -= rc;
+/* struct containing message string and an address of it's sender */
+struct addr_message {
+    sockaddr addr_sender;
+    char* message;
+
+    addr_message() {
     }
 
-    return b_rcvd;
+    addr_message(sockaddr addr_sender, char* message) {
+        this->addr_sender = addr_sender;
+        this->message = message;
+    }
+};
+
+pthread_t thread_get_header;
+pthread_t thread_listen_cmd;
+
+void get_id_from_message(uint* msg_id, char* message) {
+    memcpy(msg_id, message, MSG_ID_LEN);
 }
 
-void send_msg_by_sock(int socket, char* header, char* uname, char* msg) {
-
-    char buffer_head[HEADER_LEN];
-    char buffer_opt[OPT_UNAME_LEN + OPT_MSGLEN_LEN];
-    char buffer_msg[MSG_MAXLEN];
-
-    memset(buffer_head, 0, HEADER_LEN);
-    memset(buffer_opt, 0, OPT_UNAME_LEN + OPT_MSGLEN_LEN);
-    memset(buffer_msg, 0, MSG_MAXLEN);
-
-    memcpy(buffer_head, header, HEADER_LEN);
-
-
-    short int msg_len = 0;
-    int opt_len = 0;
-
-    opt_len += (uname == NULL) ? 0 : OPT_UNAME_LEN;
-    opt_len += (msg == NULL) ? 0 : OPT_MSGLEN_LEN;
-    msg_len += (msg == NULL) ? 0 : strlen(msg);
-
-    if (uname != NULL) {
-        memcpy(buffer_opt, uname, OPT_UNAME_LEN);
-    }
-
-    if (msg_len != 0) {
-        if (uname != NULL) {
-            memcpy(buffer_opt + OPT_UNAME_LEN, &msg_len, OPT_MSGLEN_LEN);
-        } else {
-            memcpy(buffer_opt, &msg_len, OPT_MSGLEN_LEN);
-        }
-        memcpy(buffer_msg, msg, msg_len);
-    }
-
-    send(socket, buffer_head, HEADER_LEN, 0);
-    send(socket, buffer_opt, opt_len, 0);
-    if (msg_len != 0) {
-        send(socket, buffer_msg, msg_len, 0);
-    }
+void get_header_from_message(char* header, char* message) {
+    memcpy(header, message + MSG_ID_LEN, HEADER_LEN);
 }
 
+void get_username_from_message(char* username, char* message) {
+    memcpy(username, message + MSG_ID_LEN + HEADER_LEN, OPT_USERNAME_LEN);
+}
 
-void recv_msg_by_sock(int socket, char* uname, char* buffer_msg) {
+void get_data_from_message(char* data, char* message) {
+    memcpy(data, message + MSG_ID_LEN + HEADER_LEN + OPT_USERNAME_LEN, MSG_DATA_MAXLEN);
+}
 
-    char buffer_opt[OPT_UNAME_LEN + OPT_MSGLEN_LEN];
-    memset(buffer_opt, 0, OPT_UNAME_LEN + OPT_MSGLEN_LEN);
-    if (buffer_msg != NULL) {
-        memset(buffer_msg, 0, MSG_MAXLEN);
-    }
+void compile_message(char* message, uint* msg_id, char* header, char* username, char* data) {
+    memset(message, 0, PCG_MAXLEN);
 
-    int opt_len = 0;
-    short int msg_len = 0;
+    memcpy(message, msg_id, MSG_ID_LEN);
+    memcpy(message + MSG_ID_LEN, header, HEADER_LEN);
 
-    opt_len += (uname == NULL) ? 0 : OPT_UNAME_LEN;
-    opt_len += (buffer_msg == NULL) ? 0 : OPT_MSGLEN_LEN;
-    msg_len += (buffer_msg == NULL) ? 0 : MSG_MAXLEN;
-
-    readn(socket, buffer_opt, opt_len);
-    if (uname != NULL) {
-        memcpy(uname, buffer_opt, OPT_UNAME_LEN);
-        memcpy(&msg_len, buffer_opt + OPT_UNAME_LEN, OPT_MSGLEN_LEN);
+    if (username != NULL) {
+        memcpy(message + MSG_ID_LEN + HEADER_LEN, username, OPT_USERNAME_LEN);
     } else {
-        memcpy(&msg_len, buffer_opt, OPT_MSGLEN_LEN);
+        memset(message + MSG_ID_LEN + HEADER_LEN, 0, OPT_USERNAME_LEN);
     }
 
-    if (msg_len != 0) {
-        readn(socket, buffer_msg, msg_len);
+    if (data != NULL) {
+        memcpy(message + MSG_ID_LEN + HEADER_LEN + OPT_USERNAME_LEN, data, strlen(data));
+    } else {
+        memset(message + MSG_ID_LEN + HEADER_LEN + OPT_USERNAME_LEN, 0,
+                PCG_MAXLEN - (MSG_ID_LEN + HEADER_LEN + OPT_USERNAME_LEN));
     }
 }
 
-void* inc_msg(void* arg) {
+void* incoming_msg(void* arg) {
 
-    int sock_serv = *((int*)arg);
-    char buffer_opt[OPT_UNAME_LEN + OPT_MSGLEN_LEN];
-    char buffer_msg[MSG_MAXLEN];
-    char uname[OPT_UNAME_LEN];
+    char username[OPT_USERNAME_LEN];
+    char data[MSG_MAXLEN];
 
-    recv_msg_by_sock(sock_serv, uname, buffer_msg);
+    get_username_from_message(username, received_message);
+    get_data_from_message(data, received_message);
 
-
-    printf("%s > [ %s ]\n\n", uname, buffer_msg);
+    printf("%s > [ %s ]\n\n", username, data);
 
     return NULL;
 }
 
-void* err_msg(void* arg) {
-    int sock_serv = *((int*)arg);
+void* error_msg(void* arg) {
+    int sock_serv = *((int*) arg);
     char buffer_msg[MSG_MAXLEN];
 
-    recv_msg_by_sock(sock_serv, NULL, buffer_msg);
 
     printf("ERROR: [ %s ]\n\n", buffer_msg);
 
     return NULL;
 }
 
-void* auth_me(void* arg) {
-    int rc;
+void* authorize_me(void* arg) {
 
     char header[HEADER_LEN] = HDR_CLI_AUTHORIZE;
-    char input_name[OPT_UNAME_LEN];
-    memset(input_name, 0, OPT_UNAME_LEN);
+    char username[OPT_USERNAME_LEN];
+    memset(username, 0, OPT_USERNAME_LEN);
 
-    std::cin.getline(input_name, OPT_UNAME_LEN);
+    std::cin.getline(username, OPT_USERNAME_LEN);
     std::cin.clear();
 
-    send_msg_by_sock(sockfd, header, input_name, NULL);
+    char message[PCG_MAXLEN];
+    memset(message, 0, PCG_MAXLEN);
+
+    compile_message(message, &msgid_outgoing, header, username, NULL);
+    sendto(sockfd, message, MSG_ID_LEN + HEADER_LEN + OPT_USERNAME_LEN,
+            0, &addr_server, sizeof (addr_server));
+    msgid_outgoing += MSG_ID_STEP;
 
     return NULL;
 }
 
-void* disc_me(void* arg) {
-    int rc;
-    char header[HEADER_LEN] = HDR_CLI_DISCONNECT;
+void* disconnect_me(void* arg) {
 
-    TerminateThread(h_wait_for_recv, NULL);
-    WaitForSingleObject (h_wait_for_recv, INFINITE);
-    send_msg_by_sock(sockfd, header, NULL, NULL);
+    pthread_cancel(thread_get_header);
+    pthread_join(thread_get_header, NULL);
 
-    shutdown(sockfd, SD_BOTH);
-    closesocket(sockfd);
+    shutdown(sockfd, SHUT_RDWR);
+    close(sockfd);
 
     int* rc_disc = new int(RC_CLIENT_CLOSING);
-    return (void*)rc_disc;
+
+    return (void*) rc_disc;
 }
 
 void* close_client(void* arg) {
     printf("\nclosing...\n");
 
-    void* rc = disc_me(NULL);
+    void* rc = disconnect_me(NULL);
+
     return rc;
 }
 
 void* send_msg(void* arg) {
-    
-    char buffer_head[HEADER_LEN] = HDR_CLI_RETRANSMIT_MESSAGE;
-    char input_name[OPT_UNAME_LEN];
-    char input_msg[MSG_MAXLEN];
-    memset(input_name, 0, OPT_UNAME_LEN);
-    memset(input_msg, 0, MSG_MAXLEN);
-    std::cin.getline(input_name, OPT_UNAME_LEN + OPT_MSGLEN_LEN, ' ');
-    std::cin.getline(input_msg, MSG_MAXLEN);
+
+    char header[HEADER_LEN] = HDR_CLI_RETRANSMIT_MESSAGE;
+    char username[OPT_USERNAME_LEN];
+    char data[MSG_MAXLEN];
+    memset(username, 0, OPT_USERNAME_LEN);
+    memset(data, 0, MSG_MAXLEN);
+    std::cin.getline(username, OPT_USERNAME_LEN + OPT_MSGLEN_LEN, ' ');
+    std::cin.getline(data, MSG_MAXLEN);
     std::cin.clear();
 
-    send_msg_by_sock(sockfd, buffer_head, input_name, input_msg);
+    char message[PCG_MAXLEN];
+    memset(message, 0, PCG_MAXLEN);
+    compile_message(message, &msgid_outgoing, header, username, data);
+
+    sendto(sockfd, message, MSG_ID_LEN + HEADER_LEN + OPT_USERNAME_LEN + strlen(data),
+            0, &addr_server, sizeof (addr_server));
+    msgid_outgoing += MSG_ID_STEP;
 
     return NULL;
 }
 
-void* bc_msg(void* arg) {
-    char buffer_head[HEADER_LEN] = HDR_CLI_BROADCAST_MESSAGE;
-    char input_msg[MSG_MAXLEN];
-    memset(input_msg, 0, MSG_MAXLEN);
-    std::cin.getline(input_msg, MSG_MAXLEN);
+void* send_broadcast_msg(void* arg) {
+    char header[HEADER_LEN] = HDR_CLI_BROADCAST_MESSAGE;
+    char data[MSG_MAXLEN];
+    memset(data, 0, MSG_MAXLEN);
+    std::cin.getline(data, MSG_MAXLEN);
     std::cin.clear();
 
-    send_msg_by_sock(sockfd, buffer_head, NULL, input_msg);
+    char message[PCG_MAXLEN];
+    memset(message, 0, PCG_MAXLEN);
+    compile_message(message, &msgid_outgoing, header, NULL, data);
+
+    sendto(sockfd, message, MSG_ID_LEN + HEADER_LEN + OPT_USERNAME_LEN + strlen(data),
+            0, &addr_server, sizeof (addr_server));
+    msgid_outgoing += MSG_ID_STEP;
 
     return NULL;
 }
 
-DWORD WINAPI wait_for_recv(CONST LPVOID arg) {
+void* get_header(void* arg) {
 
-    int newsockfd = *(int*)arg;
+    handlers.insert(fname_function(std::string(HDR_SRV_INCOMING_MESSAGE), &incoming_msg)); // incoming message
+    handlers.insert(fname_function(std::string(HDR_SRV_ERROR), &error_msg)); // error message
 
-    ssize_t rc;
-    char buffer[HEADER_LEN];
+    struct sockaddr addr_server_tmp;
+    uint addr_server_tmp_len = sizeof (addr_server_tmp);
+    received_message = new char[PCG_MAXLEN];
+    uint msg_id;
+    char header[HEADER_LEN];
 
-    while(1) {
+    msgid_incoming = 0;
+    msgid_outgoing = 0;
 
-        memset(buffer, 0, HEADER_LEN);
-        rc = readn(newsockfd, buffer, HEADER_LEN);
+    while (1) {
+        memset(received_message, 0, PCG_MAXLEN);
+        recvfrom(sockfd, received_message, PCG_MAXLEN, 0, &addr_server_tmp, &addr_server_tmp_len);
 
-        if (rc <= 0) {
-            shutdown(sockfd, SD_BOTH);
-            closesocket(sockfd);
-            std::cout << "connection lost\n";
+        get_id_from_message(&msg_id, received_message);
+        get_header_from_message(header, received_message);
 
-            TerminateThread(h_listen_cmd, NULL);
-            CloseHandle(overlapped.hEvent);
-            CloseHandle(h_listen_cmd);
-            WaitForSingleObject (h_listen_cmd, INFINITE);
+        char username[OPT_USERNAME_LEN];
+        char data[MSG_MAXLEN];
+        get_username_from_message(username, received_message);
+        get_data_from_message(data, received_message);
 
-            ExitThread(NULL);
+        if (msg_id != msgid_incoming + MSG_ID_STEP) {
+            msgid_incoming = msg_id;
+
+            std::cout << "[warning] package loss\n";
+        } else {
+            msgid_incoming += MSG_ID_STEP;
         }
-
-        std::string header(buffer);
         if (handlers.find(header) != handlers.end()) {
-            handlers[header]((void*)&newsockfd);
+            handlers[header](NULL);
         }
     }
-
-    return NULL;
 }
 
-DWORD WINAPI listen_cmd(CONST LPVOID arg) {
+void* listen_cmd(void* arg) {
 
-    std::map<std::string, void*(*)(void*)> handlers_cmd;
+    std::map < std::string, void*(*)(void*) > handlers_cmd;
 
     handlers_cmd.insert(fname_function(std::string(CMD_EXIT), &close_client));
-    handlers_cmd.insert(fname_function(std::string(CMD_DISCONNECT_ME), &disc_me));
-    handlers_cmd.insert(fname_function(std::string(CMD_AUTHORIZE_ME), &auth_me));
+    handlers_cmd.insert(fname_function(std::string(CMD_DISCONNECT_ME), &disconnect_me));
+    handlers_cmd.insert(fname_function(std::string(CMD_AUTHORIZE_ME), &authorize_me));
     handlers_cmd.insert(fname_function(std::string(CMD_SEND_MESSAGE), &send_msg));
-    handlers_cmd.insert(fname_function(std::string(CMD_BC_MESSAGE), &bc_msg));
+    handlers_cmd.insert(fname_function(std::string(CMD_BC_MESSAGE), &send_broadcast_msg));
 
-    while(1) {
+    while (1) {
 
         std::string input;
         std::cin >> input;
@@ -276,8 +280,9 @@ DWORD WINAPI listen_cmd(CONST LPVOID arg) {
             void* rc_handlers_cmd = handlers_cmd[input](NULL);
 
             if (rc_handlers_cmd != NULL) {
-                if (*((int*)rc_handlers_cmd) == 1) {    // checking exit condition
+                if (*((int*) rc_handlers_cmd) == 1) { // checking exit condition
                     free(rc_handlers_cmd);
+
                     return NULL;
                 }
             }
@@ -286,17 +291,8 @@ DWORD WINAPI listen_cmd(CONST LPVOID arg) {
 }
 
 int main(int argc, char *argv[]) {
-    
-    int n;
-    WSADATA wsaData;
-    n = WSAStartup(MAKEWORD(2,2), &wsaData);
-    if (n != 0) {
-        printf("WSAStartup failed: %d\n", n);
-        return 1;
-    }
 
-    handlers.insert(fname_function(std::string(HDR_SRV_INCOMING_MESSAGE), &inc_msg)); // incoming message
-    handlers.insert(fname_function(std::string(HDR_SRV_ERROR), &err_msg));            // error message
+    int n;
 
     uint16_t portno;
     struct sockaddr_in serv_addr;
@@ -309,7 +305,7 @@ int main(int argc, char *argv[]) {
 
     portno = (uint16_t) atoi(argv[2]);
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
     if (sockfd < 0) {
         perror("ERROR opening socket");
@@ -323,29 +319,31 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
 
-    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+    memset((char *) &serv_addr, 0, sizeof (serv_addr));
     serv_addr.sin_family = AF_INET;
     memcpy((char *) &serv_addr.sin_addr.s_addr, server->h_addr, (size_t) server->h_length);
     serv_addr.sin_port = htons(portno);
 
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        perror("ERROR connecting");
+    addr_server = *((struct sockaddr*) &serv_addr);
+
+    cli_addr.sin_family = AF_INET;
+    cli_addr.sin_addr.s_addr = INADDR_ANY;
+    cli_addr.sin_port = htons(CLI_PORT);
+
+    if (bind(sockfd, (struct sockaddr*) &cli_addr, sizeof (cli_addr)) < 0) {
+        perror("ERROR on binding");
         exit(1);
     }
 
-    h_wait_for_recv = CreateThread(
-            NULL, NULL, &wait_for_recv, (void*)(&sockfd), NULL, &thread_wait_for_recv);
-    h_listen_cmd = CreateThread(
-            NULL, NULL, &listen_cmd, (void*)(&sockfd), NULL, &thread_listen_cmd);
+    pthread_create(&thread_get_header, NULL, &get_header, NULL);
+    pthread_create(&thread_listen_cmd, NULL, &listen_cmd, NULL);
 
     printf("\nType 'exit' to end the program\n\n");
-    WaitForSingleObject (h_listen_cmd, INFINITE);
-    TerminateThread(h_wait_for_recv, NULL);
-    WaitForSingleObject (h_wait_for_recv, INFINITE);
+    pthread_join(thread_listen_cmd, NULL);
+    pthread_cancel(thread_get_header);
 
-    shutdown(sockfd, SD_BOTH);
-    closesocket(sockfd);
-    WSACleanup();
+    shutdown(sockfd, SHUT_RDWR);
+    close(sockfd);
 
     return 0;
 }
